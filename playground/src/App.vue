1426ec8f4065e252a209b596112e2d0a7b5ea431
cole <bernankeic@gmail.com>
2024-05-08 16:57:58 +0800

    wip: realtime preview

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import type { Scheme } from "@bernankez/theme-generator";
-import { computed } from "vue";
+import { type Scheme, hexToRGB, isColor, isShape, kebabCase } from "@bernankez/theme-generator";
+import type { CSSProperties } from "vue";
+import { computed, watchEffect } from "vue";
 import { storeToRefs } from "pinia";
 import ThemePalette from "./components/ThemePalette.vue";
+import Website from "./components/Website.vue";
 import Header from "./layout/Header.vue";
 import { isDark } from "./shared/isDark";
 import { useThemeStore } from "./store/theme";
@@ -20,11 +22,34 @@ const scheme = computed<Scheme>({
 });
 
 const { theme, cssPrefix, overrides } = storeToRefs(useThemeStore());
+
+const themeStyle = computed(() => {
+  const style: CSSProperties = {};
+  for (const key in theme.value) {
+    if (isColor(key)) {
+      style[`--${kebabCase(key)}`] = [...hexToRGB(theme.value[key][scheme.value])].join(" ");
+    } else if (isShape(key)) {
+      style[`--${kebabCase(key)}`] = theme.value[key];
+    }
+  }
+  return style;
+});
+
+watchEffect(() => {
+  for (const key in themeStyle.value) {
+    document.documentElement.style.setProperty(key, themeStyle.value[key as keyof CSSProperties] as any);
+  }
+});
 </script>
 
 <template>
-  <div>
-    <Header />
-    <ThemePalette v-model:scheme="scheme" v-model:cssPrefix="cssPrefix" :model-value="theme" @update:model-value="json => overrides = json" />
+  <div class="flex">
+    <div>
+      <Website />
+    </div>
+    <div>
+      <Header />
+      <ThemePalette v-model:scheme="scheme" v-model:cssPrefix="cssPrefix" :model-value="theme" @update:model-value="json => overrides = json" />
+    </div>
   </div>
 </template>
---
+++ playground/src/components/Website.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="p-3">
+    <div class="flex items-center justify-center gap-3">
+      <div class="text-5xl text-foreground font-bold font-italic">
+        Visualize Your Colors<br />
+        On a
+        <span class="text-primary">Real</span>
+        Site
+      </div>
+      <div class="grid grid-cols-2 shrink-0 text-8xl">
+        <div class="i-lucide:dog text-primary"></div>
+        <div class="i-lucide:bird text-secondary"></div>
+        <div class="i-lucide:carrot text-accent"></div>
+        <div class="i-lucide:cat text-primary"></div>
+      </div>
+    </div>
+    <div class="flex gap-3">
+      <button class="rounded-md bg-primary p-3 text-primary-foreground hover:bg-primary/90">
+        Getting Started
+      </button>
+      <button class="rounded-md bg-secondary p-3 text-secondary-foreground hover:bg-secondary/90">
+        Document
+      </button>
+    </div>
+  </div>
+</template>
